54ac56d8e33635a434f6e55e6e60e35dfc19815b
torres7707 <wangdong7707@gmail.com>
2025-08-22 13:50:11 +0800

    section5

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-const http = require("http");
-const routes = require("./routes");
+// const http = require("http");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const server = http.createServer((req, res) => {
-	routes.handler(req, res);
+const app = express();
+
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+
+const rootDir = require("./utils/path");
+
+// app.use((req, res, next) => {
+// 	console.log("In the middleware!");
+// 	next(); // Allows the request to continue to the next middleware in line
+// });
+// app.use("/", (req, res, next) => {
+// 	console.log("This always runs!");
+// 	next();
+// });
+
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use(bodyParser.urlencoded());
+
+app.use("/admin", adminRouter);
+app.use(shopRouter);
+
+app.use("/", (req, res, next) => {
+	res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
-server.listen(3000);
+app.listen(3000);
