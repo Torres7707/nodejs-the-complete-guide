5605685daf2f625f6e3f832292da6b176606c3b1
torres7707 <wangdong7707@gmail.com>
2025-08-25 16:17:30 +0800

    secion8

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const expressLayouts = require("express-ejs-layouts");
-const errorController = require("./controllers/error");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 const app = express();
 
-// app.engine(
-// 	"hbs",
-// 	expressHbs.engine({
-// 		layoutsDir: "views/layouts",
-// 		defaultLayout: "main-layout",
-// 		extname: "hbs",
-// 	})
-// );
-app.set("view engine", "ejs");
-app.set("views", "views");
-app.use(expressLayouts);
-app.set("layout", "layouts/main-layout");
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
--- models/product.js
+++ models/product.js
@@ -1,39 +1,40 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const getProductFromFile = (cb) => {
-	const p = path.join(
-		path.dirname(require.main.filename),
-		"data",
-		"products.json"
-	);
-	fs.readFile(p, (err, fileContent) => {
-		if (err) {
-			return cb([]);
-		}
-		try {
-			cb(JSON.parse(fileContent));
-		} catch (e) {
-			cb([]);
-		}
-	});
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
 };
 
 module.exports = class Product {
-	constructor(title) {
-		this.title = title;
-	}
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-	save() {
-		getProductFromFile((products) => {
-			products.push(this);
-			fs.writeFile(p, JSON.stringify(products), (err) => {
-				console.log(err);
-			});
-		});
-	}
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-	static fetchAll(cb) {
-		getProductFromFile(cb);
-	}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
 };
